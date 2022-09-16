fd638d6aed90daccec92b4c9896431d59216f7a0
3mrePala <palaemre27@gmail.com>
2022-09-17 07:22:05 +1000

    first commit

---
+++ style.js
@@ -0,0 +1,26 @@
+
+"your dad's fat"
+
+     
+    
+    const selectionSort = (nums) => {
+        for (let i = 0; i < nums.length; i++){
+            let currMin = i
+    
+            for (let j = i + 1; j < nums.length; j++){
+                if (nums[j] < nums[currMin]){
+                    currMin = j
+                }
+            }
+    
+            if (currMin !== i){
+                let temp = nums[i]
+                nums[i] = nums[currMin]
+                nums[currMin] = temp 
+            }
+        }
+    
+        return nums
+    
+    }
+    
